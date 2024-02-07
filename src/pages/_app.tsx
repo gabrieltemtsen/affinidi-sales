24c8812a3ca2833d3354bf16c23d12c8c13b644e
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-02-07 23:05:09 +0100

    added store features

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import Layout from "@/components/Layout";
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AppProps } from "next/app";
+import { ShoppingCartProvider } from "@/context/ShoppingCartContext";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  const client = new QueryClient();
+
+  return (
+    <ShoppingCartProvider>
+      <ChakraProvider>
+        <QueryClientProvider client={client}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </QueryClientProvider>
+      </ChakraProvider>
+    </ShoppingCartProvider>
+  );
 }
+
+export default MyApp;
